972d86c81af1d75ba3fa1ca3e27fe96dbf634d33
torsten <torsten.zielinski@gmail.com>
2025-11-15 11:33:40 +0100

    first commit
    # Conflicts:
    #	.gitignore
    #	next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'localhost', '192.168.2.22'],
+};
+
 export default nextConfig;
